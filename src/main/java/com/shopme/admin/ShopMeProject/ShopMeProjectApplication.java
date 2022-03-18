a8439d10b5bdd2f9c43e5921bc143d2fa7645326
cuongnt97 <cuong97ndc@gmail.com>
2022-03-18 15:56:21 +0700

    Re-create Shopme Project

---
+++ src/main/java/com/shopme/admin/ShopMeProject/ShopMeProjectApplication.java
@@ -0,0 +1,13 @@
+package com.shopme.admin.ShopMeProject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ShopMeProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ShopMeProjectApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/shopme/admin/ShopMeProject/ShopMeProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.shopme.admin.ShopMeProject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ShopMeProjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
