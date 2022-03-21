e2e4f52de7e9e5536715065c703626c4b8fa2199
cuongnt97 <cuong97ndc@gmail.com>
2022-03-21 17:20:28 +0700

    Commit 21/03/2022

--- ShopmeWebParents/ShopmeBackEnd/src/test/java/com/shopme/admin/user/RoleRepositoryTest.java
+++ ShopmeWebParents/ShopmeBackEnd/src/test/java/com/shopme/admin/user/RoleRepositoryTest.java
@@ -5,24 +5,23 @@ import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+import org.springframework.test.annotation.Rollback;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
 @DataJpaTest
 @AutoConfigureTestDatabase(replace = AutoConfigureTestDatabase.Replace.NONE)
+@Rollback(value = false)
 public class RoleRepositoryTest {
 
     @Autowired
     private RoleRepository repo;
 
     @Test
-    public void testCreateFirstRole(){
-        Role roleAdmin = new Role("Admin", "Manage everything");
+    public void testCreateFirstRole() {
+        Role roleAdmin = new Role("Admin1", "Manage everything");
         //repo.save(roleAdmin);
         Role savedRole = repo.save(roleAdmin);
-        System.out.println(savedRole.getId());
-        System.out.println(savedRole.getName());
-        System.out.println(savedRole.getDescription());
+        assertThat(savedRole.getId()).isGreaterThan(0);
     }
-
 }
