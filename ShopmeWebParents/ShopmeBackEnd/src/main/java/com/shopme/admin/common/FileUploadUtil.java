0e224dce9b9bcb7167f8b224ed8d64adfcfd8fd3
cuongnt97 <cuong97ndc@gmail.com>
2022-04-20 18:50:50 +0700

    Commit Update delete direct link to user's phto 20/04/2022

--- ShopmeWebParents/ShopmeBackEnd/src/main/java/com/shopme/admin/common/FileUploadUtil.java
+++ ShopmeWebParents/ShopmeBackEnd/src/main/java/com/shopme/admin/common/FileUploadUtil.java
@@ -1,5 +1,7 @@
 package com.shopme.admin.common;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.web.multipart.MultipartFile;
 
 import java.io.File;
@@ -11,9 +13,13 @@ import java.nio.file.Paths;
 import java.nio.file.StandardCopyOption;
 
 public class FileUploadUtil {
+
+    private static final Logger LOGGER = LoggerFactory.getLogger(FileUploadUtil.class);
+
     public static void saveFile(String uploadDir
                                 , String fileName
                                 , MultipartFile multipartFile) throws IOException {
+
         Path uploadPath = Paths.get(uploadDir);
         if (!Files.exists(uploadPath)){
             Files.createDirectories(uploadPath);
@@ -36,12 +42,22 @@ public class FileUploadUtil {
                     try {
                         Files.delete(file);
                     } catch (IOException e){
-                        System.out.println("Could not delete file " + file);
+                        LOGGER.error("Could not delete file " + file);
                     }
                 }
             });
         } catch (IOException e) {
-            System.out.println("Could not list directory " + dirPath);
+            LOGGER.error("Could not list directory " + dirPath);
+        }
+    }
+
+    public static void removeDir(String dir) {
+        cleanDirectory(dir);
+
+        try{
+            Files.delete(Paths.get(dir));
+        }catch (IOException e){
+            LOGGER.error("Could not remove directory " + dir);
         }
     }
 
